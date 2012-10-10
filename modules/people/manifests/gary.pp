fe079dfa7d8c8637074b6334ce10e86d45bc092c
Gary Larizza <gary@puppetlabs.com>
2012-10-10 07:42:16 -0700

    Update gary's class
    Adding packages like zsh, sizeup, dropbox, and some source URLs for
    Vagrant/Virtualbox.

--- modules/people/manifests/gary.pp
+++ modules/people/manifests/gary.pp
@@ -1,19 +1,47 @@
 class people::gary {
 
-  package { 'Vagrant-1.0.5.dmg':
+  package { 'Vagrant',
     ensure => installed,
-    source => '/Users/glarizza/src/my-boxen/modules/people/files/Vagrant-1.0.5.dmg',
+    source => 'http://files.vagrantup.com/packages/be0bc66efc0c5919e92d8b79e973d9911f2a511f/Vagrant-1.0.5.dmg',
     provider => pkgdmg,
   }
 
-  package { 'VirtualBox-4.1.23-80870-OSX.dmg':
+  package { 'Virtualbox':
     ensure => installed,
-    source => '/Users/glarizza/src/my-boxen/modules/people/files/VirtualBox-4.1.23-80870-OSX.dmg',
+    source => 'http://download.virtualbox.org/virtualbox/4.1.22/VirtualBox-4.1.23-80870-OSX.dmg',
     provider => pkgdmg,
   }
 
   package { 'tree':
     ensure => installed,
     provider => homebrew,
+  }
+
+  package { 'Dropbox':
+    source   => 'https://ddr3luum8vl5r.cloudfront.net/Dropbox%201.4.3.dmg'
+    provider => appdmg,
+  }
+
+  package { 'zsh': }
+
+  file_line { 'add zsh to /etc/shells':
+    path    => '/etc/shells',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+  }
+
+  package { 'SizeUp':
+    source   => 'http://www.irradiatedsoftware.com/download/SizeUp.zip',
+    provider => compressed_app
+  }
+
+  package { 'Sparrow':
+    source   => 'http://download.sparrowmailapp.com/appcast/Sparrow-latest.dmg'
+    provider => appdmg,
+  }
+
+  package { 'Chrome':
+    source   => 'http://dl.google.com/chrome/mac/stable/GoogleChrome.dmg',
+    provider => appdmg,
   }
 }
