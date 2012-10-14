fbcc93e7ef0b11e26ddf7fad8aeb1b3c97a5ad44
Gary Larizza <gary@puppetlabs.com>
2012-10-14 13:12:21 -0700

    Add Truecrypt
    The Truecrypt package requires you to click 'accept' before
    it will expand the DMG…which sucks. Because of this, I had
    to repackage the .mpkg into a DMG that doesn't ask you to
    accept a license.  It's up on Faro, so it needs to be in
    the custom module.

--- modules/garys_custom_setup/manifests/init.pp
+++ modules/garys_custom_setup/manifests/init.pp
@@ -7,6 +7,13 @@ class garys_custom_setup {
   # Packages #
   ############
 
+  # Truecrypt requires you to accept a license, so I had to pull out the
+  # mpkg, repackage it into a new DMG, and throw it up on Faro
+  package { 'Truecrypt':
+    source   => 'http://faro.puppetlabs.lan/gary/TrueCrypt-7.1a.dmg',
+    provider => pkgdmg,
+  }
+
   package { 'VMware Fusion':
     source   => 'http://faro.puppetlabs.lan/gary/VMware-Fusion-5.0.1-825449-light.dmg',
     provider => 'appdmg',
