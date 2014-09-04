2bc6eeb90c712d45f32bf7b15611d0e33715d59b
Gary Larizza <gary@puppetlabs.com>
2014-09-04 09:46:52 -0700

    Update puppet-osx
    Lots of new features.

--- modules/people/manifests/glarizza/config.pp
+++ modules/people/manifests/glarizza/config.pp
@@ -5,8 +5,14 @@ class people::glarizza::config (
 ) {
 
   include osx::dock::autohide
+  include osx::no_network_dsstores
+  include osx::keyboard::capslock_to_control
+  include osx::software_update
+  include osx::global::key_repeat_rate
   include osx::disable_app_quarantine
   include osx::global::disable_autocorrect
+  include osx::global::expand_save_dialog
+  include osx::global::tap_to_click
   include osx::finder::unhide_library
   include osx::finder::show_all_on_desktop
   include osx::finder::enable_quicklook_text_selection
