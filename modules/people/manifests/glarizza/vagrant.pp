e0cd37739d27770418dccc60fe854d29e3fa1eb5
Gary Larizza <gary@puppetlabs.com>
2015-01-20 13:33:03 -0800

    Add all my vagrant plugins

--- modules/people/manifests/glarizza/vagrant.pp
+++ modules/people/manifests/glarizza/vagrant.pp
@@ -3,4 +3,25 @@ class people::glarizza::vagrant {
   vagrant::plugin { 'vagrant-vmware-fusion':
     license => 'puppet:///modules/people/fusion.lic',
   }
+  vagrant::plugin { 'vagrant-auto_network':
+    version => '1.0.2'
+  }
+  vagrant::plugin { 'vagrant-pe_build':
+    version => '0.9.6'
+  }
+  vagrant::plugin { 'vagrant-oscar':
+    version => '0.4.1'
+  }
+  vagrant::plugin { 'vagrant-login':
+    version => '1.0.1'
+  }
+  vagrant::plugin { 'vagrant-multiprovider-snap':
+    version => '0.0.14'
+  }
+  vagrant::plugin { 'vagrant-share':
+    version => '1.1.2'
+  }
+  vagrant::plugin { 'vagrant-windows':
+    version => '1.6.0'
+  }
 }
