71a4e2ee279e114a736d38ad1db6b5f2f9aa2e06
Gary Larizza <gary@puppetlabs.com>
2015-12-24 09:54:25 -0800

    Update Vagrant - remove versions

--- modules/people/manifests/glarizza/vagrant.pp
+++ modules/people/manifests/glarizza/vagrant.pp
@@ -3,25 +3,11 @@ class people::glarizza::vagrant {
   vagrant::plugin { 'vagrant-vmware-fusion':
     license => 'puppet:///modules/people/fusion.lic',
   }
-  vagrant::plugin { 'vagrant-auto_network':
-    version => '1.0.2'
-  }
-  vagrant::plugin { 'vagrant-pe_build':
-    version => '0.9.6'
-  }
-  vagrant::plugin { 'vagrant-oscar':
-    version => '0.4.1'
-  }
-  vagrant::plugin { 'vagrant-login':
-    version => '1.0.1'
-  }
-  vagrant::plugin { 'vagrant-multiprovider-snap':
-    version => '0.0.14'
-  }
-  vagrant::plugin { 'vagrant-share':
-    version => '1.1.2'
-  }
-  vagrant::plugin { 'vagrant-windows':
-    version => '1.6.0'
-  }
+  vagrant::plugin { 'vagrant-auto_network': }
+  vagrant::plugin { 'vagrant-pe_build': }
+  vagrant::plugin { 'vagrant-oscar': }
+  vagrant::plugin { 'vagrant-login': }
+  vagrant::plugin { 'vagrant-multiprovider-snap': }
+  vagrant::plugin { 'vagrant-share': }
+  vagrant::plugin { 'vagrant-windows': }
 }
